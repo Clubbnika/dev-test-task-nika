d05a43aed9091e479050e130ccb9238b0255e2f6
Veronika Okhten <veronika.oxt12@gmail.com>
2025-08-15 16:35:59 +0300

    Initial commit

---
+++ src/app/(public)/cart/page.tsx
@@ -0,0 +1,13 @@
+// src/app/(public)/cart/page.tsx
+
+import React from 'react';
+
+const CartPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Your Cart</h1>
+    </div>
+  );
+};
+
+export default CartPage;
